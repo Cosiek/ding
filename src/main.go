452d9957ab4f61b1387a9a28b91e87acaab8bb5d
cosiek <witold.majchrzak@21s.pl>
2017-05-19 16:47:26 +0200

    Use os/exec and paplay to play sound

--- src/main.go
+++ src/main.go
@@ -1,7 +1,10 @@
 package main
 
 import "fmt"
+import "os/exec"
 
 func main() {
     fmt.Println("Ding!")
+    cmd := exec.Command("paplay", "/usr/share/sounds/freedesktop/stereo/complete.oga")
+    cmd.Start()
 }
