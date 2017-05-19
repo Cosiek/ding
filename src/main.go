faf185c784141b54b5ec30e240cac63596057233
cosiek <witold.majchrzak@21s.pl>
2017-05-19 16:51:03 +0200

    Read command line parameter

--- src/main.go
+++ src/main.go
@@ -1,10 +1,15 @@
 package main
 
 import "fmt"
+import "os"
 import "os/exec"
+import "strconv"
 
 func main() {
     fmt.Println("Ding!")
     cmd := exec.Command("paplay", "/usr/share/sounds/freedesktop/stereo/complete.oga")
     cmd.Start()
+
+    interval, _ := strconv.Atoi(os.Args[1])
+    fmt.Println(interval)
 }
